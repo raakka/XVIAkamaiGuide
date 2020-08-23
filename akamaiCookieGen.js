78ee759eae60b7854db80a452c8de3957e721200
raakka <jrip03@gmail.com>
2020-08-23 09:34:10 -0400

    Fix stuff later to make it pretty

--- akamaiCookieGen.js
+++ akamaiCookieGen.js
@@ -24,6 +24,7 @@ var targeturl = 'https://footlocker.com/'
 var cookies = ''
 var sensorData = ''
 
+//fix this later in the search bar
 //var fakegmaillist = []
 var fakelastnames = ["Anderson", "Ashwoon", "Aikin", "Bateman", "Bongard", "Bowers", "Boyd", "Cannon", "Cast", "Deitz", "Dewalt", "Ebner", "Frick", "Hancock", "Haworth", "Hesch", "Hoffman", "Kassing", "Knutson", "Lawless", "Lawicki", "Mccord", "McCormack", "Miller", "Myers", "Nugent", "Ortiz", "Orwig", "Ory", "Paiser", "Pak", "Pettigrew", "Quinn", "Quizoz", "Ramachandran", "Resnick", "Sagar", "Schickowski", "Schiebel", "Sellon", "Severson", "Shaffer", "Solberg", "Soloman", "Sonderling", "Soukup", "Soulis", "Stahl", "Sweeney", "Tandy", "Trebil", "Trusela", "Trussel", "Turco", "Uddin", "Uflan", "Ulrich", "Upson", "Vader", "Vail", "Valente", "Van Zandt", "Vanderpoel", "Ventotla", "Vogal", "Wagle", "Wagner", "Wakefield", "Weinstein", "Weiss", "Woo", "Yang", "Yates", "Yocum", "Zeaser", "Zeller", "Ziegler", "Bauer", "Baxster", "Casal", "Cataldi", "Caswell", "Celedon", "Chambers", "Chapman", "Christensen", "Darnell", "Davidson", "Davis", "DeLorenzo", "Dinkins", "Doran", "Dugelman", "Dugan", "Duffman", "Eastman", "Ferro", "Ferry", "Fletcher", "Fietzer", "Hylan", "Hydinger", "Illingsworth", "Ingram", "Irwin", "Jagtap", "Jenson", "Johnson", "Johnsen", "Jones", "Jurgenson", "Kalleg", "Kaskel", "Keller", "Leisinger", "LePage", "Lewis", "Linde", "Lulloff", "Maki", "Martin", "McGinnis", "Mills", "Moody", "Moore", "Napier", "Nelson", "Norquist", "Nuttle", "Olson", "Ostrander", "Reamer", "Reardon", "Reyes", "Rice", "Ripka", "Roberts", "Rogers", "Root", "Sandstrom", "Sawyer", "Schlicht", "Schmitt", "Schwager", "Schutz", "Schuster", "Tapia", "Thompson", "Tiernan", "Tisler"]
 
@@ -99,7 +100,7 @@ async function bake(notifications){
 
     const cursor = ghostCursor.createCursor(page)
 
-    //TY MUSICBOT!!!!!!! MUSICBOT IS THE GOAT!!!!!!!!!!!!!!!!! I <3 MUSICBOT!!!!
+    // TY MUSICBOT!!!!!!! MUSICBOT IS THE GOAT!!!!!!!!!!!!!!!!! I <3 MUSICBOT!!!!
     // GO BUY MELOD, FAST, GOOOOOOOOOOOOOOO!!!!!!!!!
     setInterval(async function(){
       try{
@@ -135,6 +136,8 @@ async function bake(notifications){
   })
 }
 
+/*
 for (i=0;i<4;i++){
   bake(true)
 }
+*/
